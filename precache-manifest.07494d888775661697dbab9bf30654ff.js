018a2a8db4082d24712d04a4d372a8b64b918c2a
toan_nv <nguyentoandev@gmail.com>
2018-12-18 13:38:00 +0700

    Change template

---
+++ precache-manifest.07494d888775661697dbab9bf30654ff.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "33a82ec4a08e12c2fce2876c249c7008",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "91af83addfc9ff425fb38639881cc4a9",
+    "url": "/js/popper.min.js"
+  },
+  {
+    "revision": "164500af3033d590dbb570ad1a334fd1",
+    "url": "/js/jquery-3.2.1.slim.min.js"
+  },
+  {
+    "revision": "9e928a9e3835c4f3b4aa",
+    "url": "/js/chunk-vendors.fe530641.js"
+  },
+  {
+    "revision": "c209cb0b0b0e1ae5d07f",
+    "url": "/js/chunk-39be493e.1a7b7048.js"
+  },
+  {
+    "revision": "14d449eb8876fa55e1ef3c2cc52b0c17",
+    "url": "/js/bootstrap.min.js"
+  },
+  {
+    "revision": "6b712a810ad05da32a92",
+    "url": "/js/app.3c7cd857.js"
+  },
+  {
+    "revision": "9e928a9e3835c4f3b4aa",
+    "url": "/css/chunk-vendors.c9f62199.css"
+  },
+  {
+    "revision": "c209cb0b0b0e1ae5d07f",
+    "url": "/css/chunk-39be493e.1e42ded6.css"
+  },
+  {
+    "revision": "450fc463b8b1a349df717056fbb3e078",
+    "url": "/css/bootstrap.min.css"
+  },
+  {
+    "revision": "6b712a810ad05da32a92",
+    "url": "/css/app.4ec77e63.css"
+  }
+];
